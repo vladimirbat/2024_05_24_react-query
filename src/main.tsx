b83e129fe5d01152a9996b7d052f5a3849d7c21c
Vladimir Bataller <NTT.vbataller@iberia.es>
2024-05-24 09:54:02 +0200

    feat: Config QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './main.css';
 import './reset.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 async function enableMocking() {
   const { worker } = await import('./mocks/browser');
   return worker.start();
 }
+
+const queryClient = new QueryClient()
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
-    // <React.StrictMode>
-    <App />
+    // <React.StrictMode> // Eliminado para evitar doble montado del componente en desarrollo para explicar mejor las peticiones antes de react query.
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
     // </React.StrictMode>,
   );
 });
